2c2e8447c324d10ff5d866e22df7ada8472f6659
IbrahimFadel <ibrahim.m.fadel@gmail.com>
2020-06-24 13:26:28 -0400

    start seperating home page into several pages, starting w contact us

--- js/index.js
+++ js/index.js
@@ -1,100 +1,100 @@
-// document.querySelectorAll("a").forEach(link => {
-//   // console.log(link);
-//   // console.log("h");
-//   link.onclick = e => {
-//     const navbar = document.getElementsByClassName("navbar")[0];
-//     const navbarHeight = navbar.clientHeight;
+// // document.querySelectorAll("a").forEach(link => {
+// //   // console.log(link);
+// //   // console.log("h");
+// //   link.onclick = e => {
+// //     const navbar = document.getElementsByClassName("navbar")[0];
+// //     const navbarHeight = navbar.clientHeight;
 
-//     let top;
+// //     let top;
 
-//     if(link.innerHTML == "Home") {
-//       top = document.getElementById("section-1").getBoundingClientRect().top;
-//     } else if(link.innerHTML == "About Us") {
-//       top = document.getElementById("section-2").getBoundingClientRect().top;
-//     }
+// //     if(link.innerHTML == "Home") {
+// //       top = document.getElementById("section-1").getBoundingClientRect().top;
+// //     } else if(link.innerHTML == "About Us") {
+// //       top = document.getElementById("section-2").getBoundingClientRect().top;
+// //     }
 
-//     window.scrollTo(0, top + window.pageYOffset - navbarHeight);
+// //     window.scrollTo(0, top + window.pageYOffset - navbarHeight);
+// //   }
+// // });
+
+// var controller = new ScrollMagic.Controller({
+//   globalSceneOptions: {
+//     triggerHook: 0.2,
+//     duration: "200%"
+//   },
+// });
+
+// var slides = document.querySelectorAll("section");
+
+// for (var i=0; i<slides.length; i++) {
+//   new ScrollMagic.Scene({
+//       triggerElement: slides[i],
+//     })
+//     .setPin(slides[i], {pushFollowers: false})
+//     .addTo(controller);
+// }
+
+// const controller2 =  new ScrollMagic.Controller({
+//   globalSceneOptions: {
+//     triggerHook: 0.3
 //   }
 // });
 
-var controller = new ScrollMagic.Controller({
-  globalSceneOptions: {
-    triggerHook: 0.2,
-    duration: "200%"
-  },
-});
+// new ScrollMagic.Scene({
+//   triggerElement: "#section-2",
+//   offset: -150,
+//   duration: "80%",
+// })
+// .setClassToggle("#section-2-big", "visible")
+// .addTo(controller2);
 
-var slides = document.querySelectorAll("section");
+// new ScrollMagic.Scene({
+//   triggerElement: "#section-2",
+//   duration: "80%",
+// })
+// .setClassToggle(".section-2-text", "visible")
+// .addTo(controller2);
 
-for (var i=0; i<slides.length; i++) {
-  new ScrollMagic.Scene({
-      triggerElement: slides[i],
-    })
-    .setPin(slides[i], {pushFollowers: false})
-    .addTo(controller);
-}
+// new ScrollMagic.Scene({
+//   triggerElement: "#section-2",
+//   duration: "80%",
+// })
+// .setClassToggle("#get-in-touch", "visible")
+// .addTo(controller2);
 
-const controller2 =  new ScrollMagic.Controller({
-  globalSceneOptions: {
-    triggerHook: 0.3
-  }
-});
+// new ScrollMagic.Scene({
+//   triggerElement: "#section-2",
+//   duration: "80%",
+// })
+// .setClassToggle(".section-2-button", "visible")
+// .addTo(controller2);
 
-new ScrollMagic.Scene({
-  triggerElement: "#section-2",
-  offset: -150,
-  duration: "80%",
-})
-.setClassToggle("#section-2-big", "visible")
-.addTo(controller2);
+// new ScrollMagic.Scene({
+//   triggerElement: "#section-3",
+//   offset: -150,
+//   duration: "80%",
+// })
+// .setClassToggle("#section-3-big", "visible")
+// .addTo(controller2);
 
-new ScrollMagic.Scene({
-  triggerElement: "#section-2",
-  duration: "80%",
-})
-.setClassToggle(".section-2-text", "visible")
-.addTo(controller2);
+// new ScrollMagic.Scene({
+//   triggerElement: "#section-3",
+//   duration: "80%",
+// })
+// .setClassToggle("#section-3-text", "visible")
+// .addTo(controller2);
 
-new ScrollMagic.Scene({
-  triggerElement: "#section-2",
-  duration: "80%",
-})
-.setClassToggle("#get-in-touch", "visible")
-.addTo(controller2);
+// new ScrollMagic.Scene({
+//   triggerElement: "#section-4",
+//   offset: -150,
+//   duration: "80%",
+// })
+// .setClassToggle("#section-4-big", "visible")
+// .addTo(controller2);
 
-new ScrollMagic.Scene({
-  triggerElement: "#section-2",
-  duration: "80%",
-})
-.setClassToggle(".section-2-button", "visible")
-.addTo(controller2);
-
-new ScrollMagic.Scene({
-  triggerElement: "#section-3",
-  offset: -150,
-  duration: "80%",
-})
-.setClassToggle("#section-3-big", "visible")
-.addTo(controller2);
-
-new ScrollMagic.Scene({
-  triggerElement: "#section-3",
-  duration: "80%",
-})
-.setClassToggle("#section-3-text", "visible")
-.addTo(controller2);
-
-new ScrollMagic.Scene({
-  triggerElement: "#section-4",
-  offset: -150,
-  duration: "80%",
-})
-.setClassToggle("#section-4-big", "visible")
-.addTo(controller2);
-
-new ScrollMagic.Scene({
-  triggerElement: "#section-4",
-  duration: "80%",
-})
-.setClassToggle("#contact-form", "visible")
-.addTo(controller2);
+// new ScrollMagic.Scene({
+//   triggerElement: "#section-4",
+//   duration: "80%",
+// })
+// .setClassToggle("#contact-form", "visible")
+// .addTo(controller2);
